704549a4fd5cfeceac81aaa726d686dcde875326
andyterbovich <andy.terbovich@gmail.com>
2021-07-29 12:09:11 -0600

    Initialize repo in GitHub

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BondGadgetPracticeCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
