9870d3389d8b69845d635b589614bec5e046c271
andyterbovich <andy.terbovich@gmail.com>
2021-07-29 12:42:33 -0600

    Added fake gadget data and sent to Index page

--- Controllers/GadgetsController.cs
+++ Controllers/GadgetsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BondGadgetPracticeCRUD.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BondGadgetPracticeCRUD.Controllers
@@ -10,7 +11,20 @@ namespace BondGadgetPracticeCRUD.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+
+            //generate fake data for testing view
+            List<GadgetModel> gadgets = new List<GadgetModel>();
+            gadgets.Add(new GadgetModel(0, "Gun", "Secret gun", "Moonraker", "Actor Name"));
+
+            gadgets.Add(new GadgetModel(1, "Spear", "Secret spear", "Fun Movie title", "Actor Name"));
+
+            gadgets.Add(new GadgetModel(2, "Poison", "Special poison", "Moonraker", "Brosnen"));
+
+            gadgets.Add(new GadgetModel(3, "Cell Phone", "Secret cell phone", "Moonraker", "Actor Name"));
+
+            gadgets.Add(new GadgetModel(4, "Trampoline Shoes", "Boing Boing Boing", "Casino Royale", "Craig"));
+
+            return View("Index", gadgets);
         }
     }
 }
