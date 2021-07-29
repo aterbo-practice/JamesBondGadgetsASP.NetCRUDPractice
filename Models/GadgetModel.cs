702a652b7c24b80e422dcc2366c1c915ac3ddece
andyterbovich <andy.terbovich@gmail.com>
2021-07-29 12:36:27 -0600

    Gadget properties and constructors added

--- Models/GadgetModel.cs
+++ Models/GadgetModel.cs
@@ -3,8 +3,28 @@ namespace BondGadgetPracticeCRUD.Models
 {
     public class GadgetModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AppearsIn { get; set; }
+        public string WithThisActor { get; set; }
+
         public GadgetModel()
         {
+            Id = -1;
+            Name = "Nothing";
+            Description = "Nothing yet";
+            AppearsIn = "Nowhere";
+            WithThisActor = "With no one";
+        }
+
+        public GadgetModel(int id, string name, string description, string appearsIn, string withThisActor)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            AppearsIn = appearsIn;
+            WithThisActor = withThisActor;
         }
     }
 }
